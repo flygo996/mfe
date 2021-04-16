f2a8031ffde3aa1839238728c34a8218111ac2ea
laifeipeng <1020103647@qq.com>
2021-04-16 10:46:55 +0800

    因为用了一个Nginx转发，所以__webpack_public_path__的处理需要在 window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__的基础上加上'vue2/这样的字符

--- sub-vue2/src/public-path.js
+++ sub-vue2/src/public-path.js
@@ -1,4 +1,8 @@
 if (window.__POWERED_BY_QIANKUN__) {
   // eslint-disable-next-line no-undef
-  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__
+  __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__ + 'vue2/'
+  console.log(
+    'window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__:',
+    window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__,
+  )
 }
